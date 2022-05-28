b85a737af60dcc4195e51d8fee665a95d64e5868
sbx_user1051 <sbx_user1051@169.254.160.197>
2022-05-28 09:54:33 +0000

    Sat May 28 09:54:33 UTC 2022

--- lib/solutions/HLO/hello_solution.py
+++ lib/solutions/HLO/hello_solution.py
@@ -6,12 +6,13 @@ def hello(friend_name):
     """
     Says hello to a friend
 
-    friend_name: A string showing the name of a friend
+    friend_name: A string showing a name
     """
     if type(friend_name) != str:
         raise TypeEror("friend_name should be a string")
 
     return("Hello, " + friend_name + "!")
+
 
 
 
