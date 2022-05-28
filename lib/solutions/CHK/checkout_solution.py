90f51c1b30d28b69d922801b2071cf3cc52d7ebd
sbx_user1051 <sbx_user1051@169.254.160.197>
2022-05-28 10:09:33 +0000

    Sat May 28 10:09:33 UTC 2022

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -7,8 +7,8 @@ def checkout(skus):
     """
     # Create counters of A's and B's, after summing all variables we reduce
     # using special offers
-    as = 0
-    bs = 0
+    a_s = 0
+    b_s = 0
 
     # Create a dictionary of prices
     prices_dict = {"A": 50, "B": 30, "C": 20, "D": 15}
@@ -19,18 +19,19 @@ def checkout(skus):
     # Iterate through the items
     for item in skus:
         if item == "A":
-            as += 1
+            a_s += 1
         elif item == "B":
-            bs += 1
+            b_s += 1
 
         price_counter += prices_dict[item]
 
     # Create A discount:
-    as_discount = (as // 3) * (50 * 3 - 130)
+    a_s_discount = (a_s // 3) * (50 * 3 - 130)
 
     # Create B discount:
-    bs_discount = (bs // 2) * (30 * 2 - 45)
+    b_s_discount = (b_s // 2) * (30 * 2 - 45)
 
-    price_counter = price_counter - as_discount - bs_discount
+    price_counter = price_counter - a_s_discount - b_s_discount
 
     return(price_counter)
+
