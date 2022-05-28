6c19a7ea72ed845890ce1105e1ba5c85f008c77d
sbx_user1051 <sbx_user1051@169.254.160.197>
2022-05-28 10:54:38 +0000

    Sat May 28 10:54:38 UTC 2022

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -1,6 +1,3 @@
-# noinspection PyUnusedLocal
-# skus = unicode string
-
 def checkout(skus):
     """
     skus = A string containing the SKUs of all the products in the basket
@@ -9,14 +6,16 @@ def checkout(skus):
     if type(skus) != str:
         return -1
 
-    # Create counters of A's and B's, after summing all variables we reduce
+    # Create counters of A's, B's, E's and F's. After summing all variables we reduce
     # using special offers
     a_s = 0
     b_s = 0
     e_s = 0
+    f_s = 0
+
 
     # Create a dictionary of prices
-    prices_dict = {"A": 50, "B": 30, "C": 20, "D": 15, "E": 40}
+    prices_dict = {"A": 50, "B": 30, "C": 20, "D": 15, "E": 40, "F": 10}
 
     # Create a total price object:
     price_counter = 0
@@ -24,7 +23,7 @@ def checkout(skus):
     # Iterate through the items
     for item in skus:
         # For non-implemented itemsType check:
-        if item not in ["A", "B", "C", "D", "E"]:
+        if item not in prices_dict.keys():
             return -1
         elif item == "A":
             a_s += 1
@@ -32,6 +31,8 @@ def checkout(skus):
             b_s += 1
         elif item == "E":
             e_s += 1
+        elif item == "F":
+            f_s += 1
 
         price_counter += prices_dict[item]
 
@@ -40,6 +41,13 @@ def checkout(skus):
     remainder_after_5 = a_s % 5
     a_s_discount_by_3 = (remainder_after_5 // 3) * (prices_dict["A"] * 3 - 130)
     a_s_discount = a_s_discount_by_5 + a_s_discount_by_3
+
+    # Apply F discount:
+    # Every three, because you don't pay for the third one and it doesn't count
+    # for the new discount either
+    # In other words, out of every three, one is free
+    f_s_free = (f_s // 3)
+    f_s_discount = f_s_free * prices_dict["F"]
 
     # Apply E discount (Here I assume that we remove all B's that have been free)
     b_s_free = e_s // 2
@@ -51,6 +59,10 @@ def checkout(skus):
     # Create B discount to the remaining non-free B's:
     b_s_discount = (b_s // 2) * (prices_dict["B"] * 2 - 45)
 
-    price_counter = price_counter - (a_s_discount + e_s_discount_to_b + b_s_discount)
+    price_counter = price_counter - (a_s_discount +
+                                     e_s_discount_to_b +
+                                     b_s_discount +
+                                     f_s_discount)
 
     return(price_counter)
+
