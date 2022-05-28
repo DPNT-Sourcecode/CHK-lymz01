09a47202fef2a8c43ee15ca01d171b9edb346f4c
sbx_user1051 <sbx_user1051@169.254.160.197>
2022-05-28 10:00:33 +0000

    Sat May 28 10:00:33 UTC 2022

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -1,6 +1,37 @@
-
-
 # noinspection PyUnusedLocal
 # skus = unicode string
+
 def checkout(skus):
-    raise NotImplementedError()
+    """
+    skus = A string containing the SKUs of all the products in the basket
+    """
+    # Create counters of A's and B's, after summing all variables we reduce
+    # using special offers
+    as = 0
+    bs = 0
+
+    # Create a dictionary of prices
+    prices_dict = {"A": 50, "B": 30, "C": 20, "D": 15}
+
+    # Create a total price object:
+    price_counter = 0
+
+    # Iterate through the items
+    for item in skus:
+        if item == "A":
+            as += 1
+        elif item == "B":
+            bs += 1
+
+        price_counter += prices_dict[item]
+
+    # Create A discount:
+    as_discount = (as // 3) * (50 * 3 - 130)
+
+    # Create B discount:
+    bs_discount = (bs // 2) * (30 * 2 - 45)
+
+    price_counter = price_counter - as_discount - bs_discount
+
+    return(price_counter)
+
