ca486a4e249fcd0d07e7af6d380c8a81b25badb4
sbx_user1051 <sbx_user1051@169.254.160.197>
2022-05-28 09:45:51 +0000

    Sat May 28 09:45:51 UTC 2022

--- lib/solutions/HLO/hello_solution.py
+++ lib/solutions/HLO/hello_solution.py
@@ -3,4 +3,11 @@
 # noinspection PyUnusedLocal
 # friend_name = unicode string
 def hello(friend_name):
-    raise NotImplementedError()
+    """
+    Says hello in the world
+    """
+    if type(friend_name) != str:
+        raise TypeEror("friend_name should be a string")
+
+    return("Hello" + friend_name)
+
