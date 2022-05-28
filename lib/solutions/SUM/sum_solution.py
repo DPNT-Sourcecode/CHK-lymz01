0530b3171f12dd1efdb8b98ee53bbd8082b8ffdf
sbx_user1051 <sbx_user1051@169.254.160.197>
2022-05-28 09:36:04 +0000

    Sat May 28 09:36:04 UTC 2022

--- lib/solutions/SUM/sum_solution.py
+++ lib/solutions/SUM/sum_solution.py
@@ -1,3 +1,17 @@
 # noinspection PyShadowingBuiltins,PyUnusedLocal
 def compute(x, y):
-    raise NotImplementedError()
+    """
+    x: A positive integer between 0-100
+    y: A positive integer between 0-100
+
+    Returns an integer representing the sum of both numbers
+    """
+    # Checks of type and value
+    if (type(x) != int) or (type(y)!= int):
+        raise TypeError("Both numbers should be integers")
+
+    if (x < 0) or (x > 100) or (y < 0) or (y > 100):
+        raise ValueError("Input integers should lie between 0 and 100")
+
+    return(x + y)
+
