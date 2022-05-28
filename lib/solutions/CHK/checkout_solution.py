ce2f925fdbb82e92eb2f275f4c64ca0b6e5432a2
sbx_user1051 <sbx_user1051@169.254.160.197>
2022-05-28 10:42:15 +0000

    Sat May 28 10:42:15 UTC 2022

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -13,9 +13,10 @@ def checkout(skus):
     # using special offers
     a_s = 0
     b_s = 0
+    e_s = 0
 
     # Create a dictionary of prices
-    prices_dict = {"A": 50, "B": 30, "C": 20, "D": 15}
+    prices_dict = {"A": 50, "B": 30, "C": 20, "D": 15, "E": 40}
 
     # Create a total price object:
     price_counter = 0
@@ -23,21 +24,34 @@ def checkout(skus):
     # Iterate through the items
     for item in skus:
         # For non-implemented itemsType check:
-        if item not in ["A", "B", "C", "D"]:
+        if item not in ["A", "B", "C", "D", "E"]:
             return -1
         elif item == "A":
             a_s += 1
         elif item == "B":
             b_s += 1
+        elif item == "E":
+            e_s += 1
 
         price_counter += prices_dict[item]
 
     # Create A discount:
-    a_s_discount = (a_s // 3) * (50 * 3 - 130)
+    a_s_discount_by_5 = (a_s // 5) * (prices_dict["A"] * 5 - 200)
+    remainder_after_5 = a_s % 5
+    a_s_discount_by_3 = (remainder_after_5 // 3) * (prices_dict["A"] * 3 - 130)
+    a_s_discount = a_s_discount_by_5 + a_s_discount_by_3
 
-    # Create B discount:
-    b_s_discount = (b_s // 2) * (30 * 2 - 45)
+    # Apply E discount (Here I assume that we remove all B's that have been free)
+    b_s_free = e_s // 2
+    e_s_discount_to_b = min(b_s, b_s_free) * prices_dict["B"]
 
-    price_counter = price_counter - a_s_discount - b_s_discount
+    # --- Substract those items that have been free
+    b_s -= min(b_s, b_s_free)
+
+    # Create B discount to the remaining non-free B's:
+    b_s_discount = (b_s // 2) * (prices_dict["B"] * 2 - 45)
+
+    price_counter = price_counter - (a_s_discount + e_s_discount_to_b + b_s_discount)
 
     return(price_counter)
+
